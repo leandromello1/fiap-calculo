6d3beed3a9529274c2081f341a33a2612e2b3a4c
leandromello1 <pmg.leandrotech@gmail.com>
2024-03-14 14:26:03 -0300

    Metodos com e sem retorno, com e sem parametros

--- src/Moeda.java
+++ src/Moeda.java
@@ -2,12 +2,20 @@ public class Moeda {
     public double valorReal;
     public double cotacaoDolar;
     public double quantidadeDolar;
+
     public void calcularConversao(){
+
         this.quantidadeDolar = this.valorReal/this.cotacaoDolar;
     }
-
     public double retornarCalculoConversao(){
         double dolar = this.valorReal/this.cotacaoDolar;
         return dolar;
     }
+
+    public String comprarDolar(String cpf, String nome){
+        this.calcularConversao();
+        return "O cliente com cpf: " + cpf + "\nNome: " + nome
+                + " comprou na data de hoje a quantia de $ " + this.quantidadeDolar;
+    }
+
 }
